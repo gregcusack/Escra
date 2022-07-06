ef5e38f5b022f1fa26a0ba4d8333e058bbaec158
greg <gregory.cusack@colorado.edu>
2022-07-06 14:38:15 -0600

    rm legacy files

--- Legacy/ec_docker/monitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dyn_pid=$1
-bash_pid=$2
-samples=$3
-period=$4
-filename="/home/greg/Desktop/plots/${dyn_pid}_${bash_pid}_cpu_util.csv"
-
-top -bn $samples -d $period | grep -E -w "$dyn_pid|$bash_pid" | awk '{print $9}' > "$filename"
